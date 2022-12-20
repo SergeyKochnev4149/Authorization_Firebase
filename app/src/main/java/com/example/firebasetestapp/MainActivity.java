16e28b645aae33657147b5867a91b618368a6937
Serhii Kochniev <sergeykochnev4149@gmail.com>
2022-12-20 12:40:37 +0200

    Rewrite all Java code. This update resolve the problems: - Freezing when switching to show content depending of user authentication status - When the user is authenticated he can click on "back" on mobile device and return to authentication UI without logout of firebase
    Resolve of problem
    1) Deleted classes SingInActivity, SuccessfulAuthorizationActivity
    2) Rewrite all logic in MainActivity
    3) Update signIn and logout methods

--- app/src/main/java/com/example/firebasetestapp/MainActivity.java
+++ app/src/main/java/com/example/firebasetestapp/MainActivity.java
@@ -1,38 +1,104 @@
 package com.example.firebasetestapp;
 
-import androidx.appcompat.app.AppCompatActivity;
+
+import static android.content.ContentValues.TAG;
 
 import android.content.Context;
-import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editTextEmail, editTextPassword;
+    private FirebaseAuth mAuth;
+    private TextView error;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent goToSignIn = new Intent(MainActivity.this, SignInActivity.class);
-        startActivity(goToSignIn);
+        mAuth = FirebaseAuth.getInstance();
     }
 
-    public static boolean hasConnection(final Context context)
-    {
-        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null)
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            setContentView(R.layout.activity_successful_authorization);
+        } else {
+            setContentView(R.layout.activity_sign_in);
+            editTextEmail = findViewById(R.id.editTextTextEmailAddress);
+            editTextPassword = findViewById(R.id.editTextTextPassword);
+            error = findViewById(R.id.error);
+        }
+    }
+
+
+
+    public void signIn(View view) {
+        String email = editTextEmail.getText().toString();
+        String password = editTextPassword.getText().toString();
+
+
+        if (!(email.equals("") && password.equals(""))) {
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(MainActivity.this, (OnCompleteListener<AuthResult>) task -> {
+                        if (task.isSuccessful()) {
+                            Log.d(TAG, "signInWithEmail:success");
+                            setContentView(R.layout.activity_successful_authorization);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            if (hasConnection(this))
+                                error.setText("Incorrect email or password");
+                            else {
+                                error.setText("");
+                                Toast.makeText(MainActivity.this, "No internet connection.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    });
+        } else
+            error.setText("Email or password is empty");
+    }
+
+
+
+    public void logout(View view) {
+        FirebaseAuth.getInstance().signOut();
+        setContentView(R.layout.activity_sign_in);
+    }
+
+
+
+    public static boolean hasConnection(final Context context) {
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
         NetworkInfo wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
-        if (wifiInfo != null && wifiInfo.isConnected())
-        {
+        if (wifiInfo != null && wifiInfo.isConnected()) {
             return true;
         }
         wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
-        if (wifiInfo != null && wifiInfo.isConnected())
-        {
+        if (wifiInfo != null && wifiInfo.isConnected()) {
             return true;
         }
         wifiInfo = cm.getActiveNetworkInfo();
-        if (wifiInfo != null && wifiInfo.isConnected())
-        {
+        if (wifiInfo != null && wifiInfo.isConnected()) {
             return true;
         }
         return false;
